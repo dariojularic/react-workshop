b741baf26ba2cb34d6eac8f058130c497357c232
Dario Jularic <dariojularic0@gmail.com>
2024-08-12 23:18:21 +0200

    Add div

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
           <img src={reactLogo} className="logo react" alt="React logo" />
           <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>

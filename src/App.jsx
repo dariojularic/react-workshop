8988571e4c22710121ddf1a7197b862d3330ecca
Dario Jularic <dariojularic0@gmail.com>
2024-08-13 21:09:18 +0200

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { pizzaData } from "/public/data"
+
+// kad napisem React.createElement() dobijem error React not defined. pojasnit
+// kad napisem ' u <footer> dobijem bude crveno ali nema errora - can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.eslintreact/no-unescaped-entities
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Header />
+      <Menu />
+      <Footer />
     </>
   )
 }
+
+function Pizza() {
+  return (
+    <div>
+      <img src="/public/pizzas/focaccia.jpg" alt="Pizza focaccia" />
+      <h2>{pizzaData[0].name}</h2>
+      <p>{pizzaData[0].ingredients}</p>
+    </div>
+  )
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  )
+}
+
+function Footer() {
+  // return React.createElement("footer", null, "We're currently open")
+  return <footer>{new Date().toLocaleTimeString()}. We' are currently open</footer>
+}
+
 
 export default App

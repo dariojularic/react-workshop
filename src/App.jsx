137fdd0b6b403ad99d859bcdd24a3f8f45eaa7ba
Dario Jularic <dariojularic0@gmail.com>
2024-08-24 21:32:34 +0200

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -44,21 +44,21 @@ function Header() {
 }
 
 function Menu() {
-  // const pizzas = pizzaData;
-  const pizzas = []
+  const pizzas = pizzaData;
+  // const pizzas = []
   const numPizzas = pizzas.length
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map(pizza => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
-      )}
+      ) : null}
 
 
 

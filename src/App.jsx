d402ade80ad5813cd61c044a2290a19de1a31ea7
Dario Jularic <dariojularic0@gmail.com>
2024-08-25 10:07:39 +0200

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -56,14 +56,17 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone even, all organic, all delicious.</p>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map(pizza => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone even, all organic, all delicious.</p>
+
+          <ul className="pizzas">
+            {pizzas.map(pizza => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We are still working on our menu. Please come back later</p>
       )}

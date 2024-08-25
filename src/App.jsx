db62908cdbfcdd3a7e42793b59790fc689cbf296
Dario Jularic <dariojularic0@gmail.com>
2024-08-25 20:40:59 +0200

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function Pizza({pizzaObj}) {
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   )

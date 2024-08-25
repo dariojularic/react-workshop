c0566091823c3eb419224c5468487795e135632d
Dario Jularic <dariojularic0@gmail.com>
2024-08-25 09:41:35 +0200

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,18 @@ function App() {
   )
 }
 
-function Pizza(props) {
-  console.log(props)
+function Pizza({pizzaObj}) {
+  console.log(pizzaObj)
 
-  if (props.pizzaObj.soldOut) return null;
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.photoName} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.photoName} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   )

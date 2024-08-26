ede7d2edb41e978f1e12c733a7309310e08dc0b9
Dario Jularic <dariojularic0@gmail.com>
2024-08-26 19:56:04 +0200

    Refactor code

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,20 +4,11 @@ import React, { useState } from "react"
 const Counter = () => {
   const [count, setCount] =  useState(0);
 
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-
-  const handleDecrement = () => {
-    setCount(count - 1);
-  };
-
-
   return (
     <React.Fragment>
       <p>The count is: {count} </p>
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </React.Fragment>
   )
 }

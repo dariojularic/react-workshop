0cb5b13b7c341baef82b4a4cca9de0aaa0a18ee5
Dario Jularic <dariojularic0@gmail.com>
2024-08-26 18:58:59 +0200

    Create two buttons

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 
 const Counter = () => {
+  const count = 0;
+
   return (
-    
+    <React.Fragment>
+      <p>The count is: {count} </p>
+      <button>Increment</button>
+      <button>Decrement</button>
+    </React.Fragment>
   )
 }
 

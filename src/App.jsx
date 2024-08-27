90c998c8e90b935862a97f845c25cac4a944d7c8
Dario Jularic <dariojularic0@gmail.com>
2024-08-27 21:22:51 +0200

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,46 @@ import React from "react";
 import "./App.css";
 import Employee from "./Employee";
 
+const employeeInfo = [
+  {
+    firstName: "Jack",
+    lastName: "Smith",
+    age: "23"
+  },
+  {
+    firstName: "Jonny",
+    lastName: "F",
+    age: "22"
+  },
+  {
+    firstName: "Mick",
+    lastName: "A",
+    age: "32"
+  },
+  {
+    firstName: "Top",
+    lastName: "G",
+    age: "45"
+  }
+];
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Company Directory</h1>
-        <Employee firstName="Jack" lastName="Smith" />
-        <Employee firstName="Jonny" lastName="D" />
-        <Employee firstName="Rio" lastName="A"/>
-        <Employee firstName="Marco" lastName="B"/>
+        {employeeInfo.map(employee => {
+          // const {firstName, lastName, age} = employee;
+          return (
+            <Employee {...employee} />
+          )
+        })}
+
+        {/* <Employee firstName="Jack" lastName="Smith" age="26"/>
+        <Employee firstName="Jonny" lastName="D" age="36"/>
+        <Employee firstName="Rio" lastName="A" age="36"/>
+        <Employee firstName="Marco" lastName="B" age="26"/> */}
       </header>
     </div>
   )
--- src/Employee.jsx
+++ src/Employee.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Employee = (props) => {
   console.log(props)
+  const {firstName, lastName, age} = props
   return (
-    <h1>Employee Name: {props.firstName}</h1>
+    <h1>Employee Name: {firstName} {lastName} who is age {age}</h1>
   )
 }
 

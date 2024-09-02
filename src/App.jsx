efb1080928b88581663bbd4d7e8d925ee4ce182d
Dario Jularic <dariojularic0@gmail.com>
2024-09-02 20:01:48 +0200

    Add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import Counter from "./Counter";
 import "./App.css";
+import TodoList from "./TodoList";
 
 function App() {
  const [showCounter, setShowCounter] = useState(false);
  return (
-  <div className="App">
-    <h1>Our App</h1>
-    <button onClick={() => setShowCounter(!showCounter)}>
-      {showCounter ? "Hide Counter" : "Show Counter"}
-    </button>
-    {showCounter && <Counter />}
-  </div>
+  // <div className="App">
+  //   <h1>Our App</h1>
+  //   <button onClick={() => setShowCounter(!showCounter)}>
+  //     {showCounter ? "Hide Counter" : "Show Counter"}
+  //   </button>
+  //   {showCounter && <Counter />}
+  // </div>
+  <TodoList/>
  )
 }
 

b426d4d6fb9a2cf9a7a3e975db849542cb588056
Dario Jularic <dariojularic0@gmail.com>
2024-09-02 12:47:57 +0200

    Refactor code

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 
 const Counter = () => {
   const [count, setCount] =  useState(0);
 
+  useEffect(() => {}, [])
+
   return (
-    <React.Fragment>
+    <div>
+      <h6> Counter</h6>
       <p>The count is: {count} </p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
-    </React.Fragment>
+    </div>
   )
 }
 
